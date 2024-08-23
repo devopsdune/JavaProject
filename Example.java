732429037a5d4c0d683ac2b0ccc4061d4ab4002e
prashanth <nocturnaldevops@gmail.com>
2024-08-23 20:22:00 +0530

    new commit

--- Example.java
+++ Example.java
@@ -6,6 +6,6 @@ System.out.println("hello world");
 System.out.println("Hello again");
   System.out.println("hello again from Github");
 System.out.println("This is a  statement from local server");
-  SOP(:"Newchnage");
+  SOP("Newchnage");
 }
 }
